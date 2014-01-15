42c02274217a5e1738e37bc82ef24a475a211c9e
Broveloper <brocoin@gmail.com>
2014-01-14 20:32:44 -0800

    styles and fonts

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,11 +1,5 @@
 var song = new Audio('http://brocoin.s3.amazonaws.com/turn-down-for-what.ogg');
-song.volume = 0.5;
-
+song.volume = 0.3;
 if (typeof song.loop == 'boolean') song.loop = true;
-else {
-    song.addEventListener('ended', function() {
-        this.currentTime = 0;
-        this.play();
-    }, false);
-}
+else song.addEventListener('ended', function() { this.currentTime = 0; this.play(); }, false);
 song.play();
