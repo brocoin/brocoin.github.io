23db64636d6b56ddcd0396ef5bd8fc1184868c61
Broveloper <brocoin@gmail.com>
2014-01-15 22:10:21 -0800

    add helping out page, use static.bro-coin.com CDN

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,4 +1,4 @@
-var song = new Audio('http://brocoin.s3.amazonaws.com/turn-down-for-what.ogg');
+var song = new Audio('http://static.bro-coin.com/turn-down-for-what.ogg');
 song.volume = 0.3;
 if (typeof song.loop == 'boolean') song.loop = true;
 else song.addEventListener('ended', function() { this.currentTime = 0; this.play(); }, false);
